2d5cb6ac35bc5287081b0536df0cefa60258bb0a
AlexeyIvanov1981 <lelikus81@yandex.ru>
2023-01-22 21:36:54 +0300

    Homework is done v.2)))

--- src/main/java/ru/home/Main.java
+++ src/main/java/ru/home/Main.java
@@ -3,8 +3,8 @@ package ru.home;
 public class Main {
     public static void main(String[] args) {
         Ints calc = new IntsCalculator();
-        System.out.println(calc.sum(1,2));
-        System.out.println(calc.mult(10,20));
-        System.out.println(calc.pow(1100,2000));
+        System.out.println(calc.sum(1, 2));
+        System.out.println(calc.mult(10, 20));
+        System.out.println(calc.pow(1100, 2000));
     }
 }
